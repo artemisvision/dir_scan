4f355467ba21a90239f2973bf7c078f5fad3965f
Eric Huff <thegreaterici@gmail.com>
2018-07-25 21:06:01 +0000

    Changes to the interface

--- build.rs
+++ build.rs
@@ -1,11 +1,31 @@
 extern crate cbindgen;
 
 use std::env;
+use std::path::PathBuf;
+use cbindgen::Config;
 
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
-    cbindgen::generate(&crate_dir)
-        .unwrap()
-        .write_to_file("dir_scan.h");
+    let package_name = env::var("CARGO_PKG_NAME").unwrap();
+    let output_file = target_dir()
+        .join(format!("{}.hpp", package_name))
+        .display()
+        .to_string();
+
+    println!("AAAA--- {:?}", crate_dir);
+    match cbindgen::generate(&crate_dir) {
+        Ok(e) => {e.write_to_file(&output_file);}
+        Err(err) => {
+            println!("Something went wrong {}", err);
+        }
+    }
+}
+
+fn target_dir() -> PathBuf {
+    if let Ok(target) = env::var("CARGO_TARGET_DIR") {
+        PathBuf::from(target)
+    } else {
+        PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,16 @@ extern crate libc;
 use libc::{c_char, size_t};
 use std::ffi::{CStr, CString};
 use std::fs;
+use std::path::Path;
 
 #[repr(C)]
 pub struct FileList {
     list: *mut *mut c_char,
     len: size_t,
 }
+
 #[no_mangle]
-pub extern "C" fn scan_dir(working_dir: *const c_char, files: *mut *mut c_char) -> FileList {
+pub extern "C" fn scan_dir(working_dir: *const c_char) -> *mut FileList {
     let c_str = unsafe {CStr::from_ptr(working_dir)};
     let foldername = c_str.to_str().unwrap();
     let r_files = fs::read_dir(foldername).unwrap();
@@ -24,24 +26,58 @@ pub extern "C" fn scan_dir(working_dir: *const c_char, files: *mut *mut c_char) 
 
     let vecptr = paths.as_mut_ptr();
     std::mem::forget(paths);
-    FileList {
+    Box::into_raw(Box::new(FileList {
         list: vecptr,
         len: ret,
+    }))
+}
+
+#[no_mangle]
+pub extern "C" fn strip_file_ext(file_path: *const c_char) -> *mut c_char {
+    if file_path.is_null() 
+    {
+        CString::new("").unwrap().into_raw()
+    }
+    else {
+        let r_str = unsafe{CStr::from_ptr(file_path)}.to_str().unwrap();
+        let path = Path::new(r_str);
+        let filename_no_ext = path.file_stem().unwrap().to_str().unwrap();
+        CString::new(filename_no_ext).unwrap().into_raw()
     }
 }
 
 #[no_mangle]
-pub extern "C" fn free(files: *mut *mut c_char, len: size_t) {
+pub extern "C" fn create_all_dirs(path: *const c_char) {
+    if path.is_null() {
+        // TODO return error
+        return;
+    }
+    else {
+        let r_str = unsafe{ CStr::from_ptr(path)}.to_str().unwrap();
+        let path = Path::new(r_str).parent().unwrap();
+        match std::fs::create_dir_all(path) {
+            Ok(_) => (),
+            Err(e) => println!("Error creating directories: {:?}", e)
+        }
+
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn free_filelist(filelist: *mut FileList) {
     unsafe {
-        if files.is_null() {return;}
-        let f: Vec<*mut c_char> = Vec::from_raw_parts(files, len, len);
-        for i in f {
-            free_string(i);
+        if filelist.is_null() {return;}
+        let f = Box::from_raw(filelist);
+        let list = Vec::from_raw_parts(f.list, f.len, f.len);
+        for i in 0..f.len {
+            free_string(list[i]);
         }
     }
 }
 
-fn free_string(s: *mut libc::c_char) {
+
+#[no_mangle]
+pub extern "C" fn free_string(s: *mut libc::c_char) {
     unsafe {
         if s.is_null() {return;}
         CString::from_raw(s)
@@ -50,8 +86,17 @@ fn free_string(s: *mut libc::c_char) {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn strip_file_ext_test() {
+        let file = String::from("some.file");
+        let f = CString::new(file).unwrap().into_raw();
+        let stripped_file = strip_file_ext(f);
+        let assert_file = unsafe{CStr::from_ptr(stripped_file)}.to_str().unwrap();
+        assert_eq!("some", assert_file);
+        unsafe {
+            CString::from_raw(stripped_file);
+            CString::from_raw(f);
+        }
     }
 }
